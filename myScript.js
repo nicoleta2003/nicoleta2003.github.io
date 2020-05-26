ca81c5729e010f826e4fb72d614aedb9ed784fdd
nicoleta2003 <65164559+nicoleta2003@users.noreply.github.com>
2020-05-26 13:14:18 +0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -5,8 +5,8 @@ function mesaj2() {
 alert("De ce apeși,mă??) ;
 } 
 function mesaj3() {
-Let intrebare=prompt(" Ești minor? ") ;
-document.getElementById("vârstă"). innerHTML=intrebare;
+let intrebare=prompt(" Ești minor? ") ;
+document.getElementById("vârstă").innerHTML=intrebare;
 
 
 } 
